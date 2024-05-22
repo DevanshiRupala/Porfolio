13da97fdd628f88f1ac3093167b39814df31b207
DevanshiRupala <rupaladevanshi@gmail.com>
2024-05-22 22:46:18 +0530

    changes

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -2,23 +2,20 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
-
 import Particle from '../components/Particle'
 import pdf from "../assets/Devanshi.pdf"
-
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = 'https://drive.google.com/file/d/1m415f_ZDYe6XOQQb_Ka-RgfR3AxPVQ4-/view?usp=drive_link'
+const resumeLink = 'https://github.com/DevanshiRupala/Porfolio/blob/main/portfolio/src/assets/Devanshi.pdf'
 
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
     setWidth(window.innerWidth);
   }, []);
 
@@ -30,7 +27,7 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
